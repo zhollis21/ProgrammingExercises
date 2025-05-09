6616f620f3585cfc0a89048495680c58fb4acba2
Zach Hollis <Zach.Hollis@claytonhomes.com>
2025-05-09 14:19:52 -0400

    In Progress Check In

---
+++ ProjectEuler/ProjectEuler/Problems.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler;
+
+public class Problems
+{
+    /// <summary>
+    /// Problem 1: Multiples of 3 and 5
+    /// </summary>
+    public static int Problem1()
+    {
+        const string Problem1Summary = @"
+            If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+            we get 3, 5, 6, and 9. The sum of these multiples is 23. Find the sum of all 
+            the multiples of 3 or 5 below 1000.";
+
+        Console.WriteLine(Problem1Summary);
+
+        int
+
+        Console.WriteLine(sum);
+        return sum;
+    }
+}
